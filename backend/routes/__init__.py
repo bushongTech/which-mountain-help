d3643dd4f760b610601395c4884ac15771726bb2
bushongTech <132151423+bushongTech@users.noreply.github.com>
2025-07-12 08:24:03 -0500

    updates

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -2,9 +2,9 @@ from .cure_routes import router as cure_router
 from .liner_routes import router as liner_router
 from .liner_application_routes import router as liner_application_router
 from .material_routes import router as material_router
-from .single_run_routes import router as single_run_router
 from .tdms_uploader_routes import router as tdms_uploader_router
-from .tensile_dashboard_routes import router as tensile_dashboard_router
+# from .single_run_routes import router as single_run_router
+# from .tensile_dashboard_routes import router as tensile_dashboard_router
 
 # Aggregate all routers into a list for easy inclusion in main.py
 routers = [
@@ -12,7 +12,6 @@ routers = [
     liner_router,
     liner_application_router,
     material_router,
-    single_run_router,
     tdms_uploader_router,
-    tensile_dashboard_router,
+
 ]
